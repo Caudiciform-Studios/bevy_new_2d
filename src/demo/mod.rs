c72e80ab03fdcc642c5b6ad5c3f6f8ee78005dea
Alec Deason <alec@tinycountry.com>
2025-02-14 13:53:59 -0800

    Remove bevy_common_assets dependency

--- src/demo/mod.rs
+++ src/demo/mod.rs
@@ -3,10 +3,13 @@
 //! Feel free to change the logic found here if you feel like tinkering around
 //! to get a feeling for the template.
 
-use bevy::prelude::*;
-use bevy_common_assets::ron::RonAssetPlugin;
+use bevy::{
+    asset::{io::Reader, AssetLoader, LoadContext},
+    prelude::*,
+};
 
 use crate::asset_tracking::LoadResource;
+use thiserror::Error;
 
 mod animation;
 pub mod level;
@@ -14,8 +17,7 @@ mod movement;
 pub mod player;
 
 pub(super) fn plugin(app: &mut App) {
-    app.add_plugins((
-        RonAssetPlugin::<Config>::new(&[".ron"]),
+    app.register_asset_loader(ConfigAssetLoader).add_plugins((
         animation::plugin,
         movement::plugin,
         player::plugin,
@@ -32,4 +34,42 @@ pub(super) fn plugin(app: &mut App) {
 #[derive(Debug, Clone, Asset, Reflect, Resource, serde::Deserialize)]
 pub struct Config {
     max_player_speed: f32,
+}
+
+#[derive(Default)]
+struct ConfigAssetLoader;
+
+#[non_exhaustive]
+#[derive(Debug, Error)]
+enum ConfigAssetLoaderError {
+    /// An [IO](std::io) Error
+    #[error("Could not load asset: {0}")]
+    Io(#[from] std::io::Error),
+    /// A [RON](ron) Error
+    #[error("Could not parse RON: {0}")]
+    RonSpannedError(#[from] ron::error::SpannedError),
+    #[error(transparent)]
+    LoadDirectError(#[from] bevy::asset::LoadDirectError),
+}
+
+impl AssetLoader for ConfigAssetLoader {
+    type Asset = Config;
+    type Settings = ();
+    type Error = ConfigAssetLoaderError;
+    async fn load(
+        &self,
+        reader: &mut dyn Reader,
+        _settings: &(),
+        _load_context: &mut LoadContext<'_>,
+    ) -> Result<Self::Asset, Self::Error> {
+        let mut bytes = Vec::new();
+        reader.read_to_end(&mut bytes).await?;
+        let config = ron::de::from_bytes::<Config>(&bytes)?;
+
+        Ok(config)
+    }
+
+    fn extensions(&self) -> &[&str] {
+        &["ron"]
+    }
 }
